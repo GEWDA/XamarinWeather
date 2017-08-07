da706d12caad81078b7c82c7c3e9b71a8e2b243c
Judah Parsons <GEWDDEWD9@hotmail.com>
2017-08-08 10:35:48 +1200

    initial commit

--- XamarinWeather/Resources/Resource.Designer.cs
+++ XamarinWeather/Resources/Resource.Designer.cs
@@ -57,6 +57,37 @@ namespace XamarinWeather
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int LayoutHTemperature = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int LayoutV = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int spinner1 = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView2 = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
